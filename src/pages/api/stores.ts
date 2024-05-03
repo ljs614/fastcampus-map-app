9a443e1063d98ba81f3f39af8e1b6f6a11c3a3d9
이정섭 <matthew@jslee.local>
2024-05-03 14:25:11 +0900

    feat: 맛집목록 pagination 구현(강의)

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -1,15 +1,27 @@
-import { StoreType } from "@/interface";
+import { StoreApiResponse } from "@/interface";
 import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreType[]>
+  res: NextApiResponse<StoreApiResponse>
 ) {
   const prisma = new PrismaClient();
+
+  const { page = "1" }: { page?: string } = req.query;
+  const skipPage = parseInt(page) - 1;
+
+  const count = await prisma.store.count();
   const stores = await prisma.store.findMany({
     orderBy: { id: "desc" },
+    take: 10,
+    skip: skipPage * 10,
   });
 
-  res.status(200).json(stores);
+  res.status(200).json({
+    page: parseInt(page),
+    data: stores,
+    totalCount: count,
+    totalPage: Math.ceil(count / 10),
+  });
 }
--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,17 +1,22 @@
 import Loading from "@/components/Loading";
-import { StoreType } from "@/interface";
+import Pagination from "@/components/Pagination";
+import { StoreApiResponse } from "@/interface";
 import axios from "axios";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useQuery } from "react-query";
 
 export default function StoreListPage() {
+  const router = useRouter();
+  const { page = "1" }: any = router.query;
   const {
     data: stores,
     isLoading,
     isError,
-  } = useQuery("stores", async () => {
-    const { data } = await axios("/api/stores");
-    return data as StoreType[];
+  } = useQuery(`stores-${page}`, async () => {
+    const { data } = await axios(`/api/stores?page=${page}`);
+    return data as StoreApiResponse;
   });
 
   if (isError) {
@@ -28,7 +33,7 @@ export default function StoreListPage() {
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.map((store, index) => (
+          stores?.data?.map((store, index) => (
             <li className="flex justify-between gap-x-6 py-5" key={index}>
               <div className="flex gap-x-4">
                 <Image
@@ -61,6 +66,9 @@ export default function StoreListPage() {
           ))
         )}
       </ul>
+      {stores?.totalPage && (
+        <Pagination total={stores?.totalPage} page={page} />
+      )}
     </div>
   );
 }
